5ddca4ce5f40648de5bf55a759ecfe1c49745cdf
Unknown <wengpeter2006@gmail.com>
2018-12-20 15:55:00 +0800

    Boss.vue 大綱完成

--- src/components/Boss.vue
+++ src/components/Boss.vue
@@ -26,7 +26,7 @@
 </style>
 <template>
     <div class="layout">
-        <Layout>
+        <Layout :style="{minHeight: '100vh'}">
             <Header>
                 <Menu mode="horizontal" theme="dark" active-name="1">
                     <div class="layout-logo">
@@ -46,6 +46,14 @@
                 <Layout :style="{padding: '0 36px 36px'}">
                     <br>
                     <Content :style="{padding: '24px', minHeight: '450px', background: '#fff'}">
+                        <Row>
+                            <Col span="3">
+                                 <Table height="200" :columns="jobcolumns" :data="job"></Table>
+                            </Col>
+                            <Col span="21">
+                                <Table border :columns="workercolumns" :data="wocker"></Table>
+                            </Col>
+                        </Row>
                     </Content>
                     <Footer class="layout-footer-center">工程師 &copy; 求職網</Footer>
                 </Layout>
@@ -53,3 +61,116 @@
         </Layout>
     </div>
 </template>
+<script>
+export default {
+    data(){
+        return{
+            jobcolumn: [
+                {
+                    title:'職缺',
+                    key:'job'
+                },
+                {
+                    title:'動作',
+                    key:'jobaction'
+                },
+            ],
+            workercolumns: [
+                    {
+                        title: 'Name',
+                        key: 'name',
+                        render: (h, params) => {
+                            return h('div', [
+                                h('Icon', {
+                                    props: {
+                                        type: 'person'
+                                    }
+                                }),
+                                h('strong', params.row.name)
+                            ]);
+                        }
+                    },
+                    {
+                        title: 'Age',
+                        key: 'age'
+                    },
+                    {
+                        title: 'Address',
+                        key: 'address'
+                    },
+                    {
+                        title: 'Action',
+                        key: 'action',
+                        width: 150,
+                        align: 'center',
+                        render: (h, params) => {
+                            return h('div', [
+                                h('Button', {
+                                    props: {
+                                        type: 'primary',
+                                        size: 'small'
+                                    },
+                                    style: {
+                                        marginRight: '5px'
+                                    },
+                                    on: {
+                                        click: () => {
+                                            this.show(params.index)
+                                        }
+                                    }
+                                }, 'View'),
+                                h('Button', {
+                                    props: {
+                                        type: 'error',
+                                        size: 'small'
+                                    },
+                                    on: {
+                                        click: () => {
+                                            this.remove(params.index)
+                                        }
+                                    }
+                                }, 'Delete')
+                            ]);
+                        }
+                    }
+                ],
+                
+        
+        
+         wocker: [
+                    {
+                        name: 'John Brown',
+                        age: 18,
+                        address: 'New York No. 1 Lake Park'
+                    },
+                    {
+                        name: 'Jim Green',
+                        age: 24,
+                        address: 'London No. 1 Lake Park'
+                    },
+                    {
+                        name: 'Joe Black',
+                        age: 30,
+                        address: 'Sydney No. 1 Lake Park'
+                    },
+                    {
+                        name: 'Jon Snow',
+                        age: 26,
+                        address: 'Ottawa No. 2 Lake Park'
+                    }
+                ]
+            }
+    },
+    methods: {
+            show (index) {
+                this.$Modal.info({
+                    title: 'User Info',
+                    content: `Name：${this.data6[index].name}<br>Age：${this.data6[index].age}<br>Address：${this.data6[index].address}`
+                })
+            },
+            remove (index) {
+                this.data6.splice(index, 1);
+            }
+        }
+}
+</script>
