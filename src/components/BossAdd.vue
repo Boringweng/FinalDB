e0ee32b6d915c516e1bc7a99f37b67a288d834a7
peach <peachloveair@gmail.com>
2018-12-24 01:06:48 +0800

    統一 header 固定

--- src/components/BossAdd.vue
+++ src/components/BossAdd.vue
@@ -1,96 +1,245 @@
 <style scoped>
-input {padding:8px 15px; background:#ccc; border:0 none;
--webkit-border-radius: 5px;
-border-radius: 5px; }
+input {
+  padding: 8px 15px;
+  background: white;
+  border: 1px;
+  border-style: solid;
+  border-color:rgb(221, 209, 209) ;
+  -webkit-border-radius: 5px;
+  border-radius: 5px;
+}
 .button {
-    background-color: #4CAF50; /* Green */
-    border: none;
-    padding: 5px 32px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 15px;
-    margin: 3px 3px;
-    -webkit-transition-duration: 0.4s; /* Safari */
-    transition-duration: 0.4s;
-    cursor: pointer;
+  background-color: #4caf50; /* Green */
+  border: none;
+  padding: 5px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 15px;
+  margin: 3px 3px;
+  -webkit-transition-duration: 0.4s; /* Safari */
+  transition-duration: 0.4s;
+  cursor: pointer;
 }
 .button1 {
-    background-color: #4CAF50;
-    color: black;
-    border: 2px solid #4CAF50;
+  background-color: rgb(108, 197, 218);
+  color: black;
+  border: 2px solid rgb(108, 197, 218);
 }
 .button1:hover {
-    background-color: gray;
-    color: rgb(0, 0, 0);
+  background-color: gray;
+  color: rgb(0, 0, 0);
 }
-.layout{
-    border: 1px solid #d7dde4;
-    background: #f5f7f9;
-    position: relative;
-    border-radius: 4px;
-    overflow: hidden;
+.layout {
+  border: 1px solid #d7dde4;
+  background: #f5f7f9;
+  position: relative;
+  border-radius: 4px;
+  overflow: hidden;
 }
-.layout-nav{
-    width: 150px;
-    margin: 0 auto;
-    margin-right: 20px;
+.layout-nav {
+  width: 150px;
+  margin: 0 auto;
+  margin-right: 20px;
 }
-.layout-footer-center{
-    text-align: center;
+.layout-footer-center {
+  text-align: center;
 }
-.layout-logo{
-    width: 100px;
-    height: 30px;
-    border-radius: 3px;
-    float: left;
-    position: relative;
-    top: 5px;
-    left: 20px;
+.layout-logo {
+  width: 100px;
+  height: 30px;
+  border-radius: 3px;
+  float: left;
+  position: relative;
+  top: 5px;
+  left: 20px;
 }
-#exp{
-    text-align: center;
+.layout-header{
+  top: 0px;
+  width: 100%;
+  position: fixed;
+  z-index:10;
+}
+#exp {
+  text-align: center;
 }
 </style>
 <template>
-    <div class="layout">
-        <Layout>
-            <Header>
-                <Menu mode="horizontal" theme="dark" active-name="1">
-                    <div class="layout-logo">
-                        <font face="DFKai-sb" size="5" style="color:white"><b>求職網</b></font>
-                    </div>
-                    <div class="layout-nav">
-                        <MenuItem name="1">
-                        <router-link to="Worker">
-                            <Icon type="md-close" style="color:white"></Icon>
-                            <font face="DFKai-sb" size="3" style="color:white"><b>取消</b></font>
-                        </router-link>
-                        </MenuItem>
-                    </div>
-                </Menu>
-            </Header>
-            <Layout>
-                <Layout :style="{padding: '0 36px 36px'}">
-                    <Content :style="{margin: '50px 80px ', background: '#fff', minHeight: '600px'}">
-                        <div id="exp">
-                            <font face="DFKai-sb" size="5">職缺</font><br><br>
-                            <font face="DFKai-sb" size="3"><b>薪水待遇:</b></font> <input type="text" placeholder="請輸入身分證ID..."><br><br>
-                            <font face="DFKai-sb" size="3"><b>所需職位:</b></font> <input type="text" placeholder="請輸入期望薪水..."><br><br>
-                            <font face="DFKai-sb" size="3"><b>所需技能:</b></font> <input type="text" placeholder="請輸入所申請部門..."><br><br>
-                            <font face="DFKai-sb" size="3"><b>技能:</b></font> <input type="text" placeholder="請輸入技能..."><br><br>
-                            <div id="button_check">
-                                <router-link to="/">
-                                    <button class="button button1"><font face="DFKai-sb" size="4">完成</font></button>
-                                </router-link>
-                            </div>
-                        </div>
-                    </Content>
-                </Layout>
-            </Layout>
+  <div class="layout">
+    <Layout style="{minHeight: '100vh'}">
+    <div class="layout-header">
+      <Header>
+        <Menu mode="horizontal" theme="dark" active-name="1">
+          <div class="layout-logo">
+            <router-link to="/">
+              <font face="DFKai-sb" size="6" style="color:yellow">
+                <b>求職網</b>
+              </font>
+            </router-link>
+          </div>
+          <div class="layout-nav">
+             
+          </div>
+        </Menu>
+      </Header>
+      </div>
+      <Layout>
+        <Layout :style="{padding: '0 36px 36px'}">
+          <Content :style="{margin: '50px 80px ', background: '#fff', minHeight: '600px'}">
+            <div id="exp">
+              <br>
+              <font face="DFKai-sb" size="8">
+                <b>職缺</b>
+              </font>
+              <br>
+              <br>
+              <br>
+              <br>
+              <br>
+              <font face="DFKai-sb" size="3">
+                <b>所需職位:</b>
+              </font>
+              <Select v-model="cPos" style="width:173px">
+                <Option v-for="item in pos" :value="item.value" :key="item.value">{{ item.label }}</Option>
+              </Select>
+              <br>
+              <br>
+              <br>
+              <font face="DFKai-sb" size="3">
+                <b>薪水待遇:</b>
+              </font>
+              <input type="text"  v-model="Salary" placeholder="請輸入薪水待遇...">
+              <br>
+              <br>
+              <br>
+              <template id="expinput"> 
+                <div v-for="(sk, index) in cNeededSkill" v-bind:key="index">
+                    
+                    <font v-if="index==0" face="DFKai-sb" size="3">
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>技能:</b>
+                    </font>
+                    <font v-else>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</font>
+                    <Select v-model="cNeededSkill[index]" style="width:173px">
+                      <Option v-for="item in Skill" :value="item.value" :key="item.value">{{ item.label }}</Option>
+                    </Select>
+                    <input type="button" value="刪除技能" v-on:click="delbox(index)" id="del"><br><br>
+                    <br>
+                </div>
+                <br><br>
+                <input type="button" value="新增技能" v-on:click="addbox" id="add"><br><br>
+              </template>
+              <br>
+              <br>
+              <br>
+              <br>
+              <br>
+              <div id="button_check">
+                <router-link to="Boss">
+                  <Button type="primary" class="button button1">取消</Button>
+                </router-link>
+                <router-link to="/Boss">
+                  <Button type="primary" class="button button1" @click="putwork()">完成</Button>
+                </router-link>
+              </div>
+            </div>
+          </Content>
         </Layout>
-    </div>
+      </Layout>
+    </Layout>
+  </div>
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      lines: [],
+      Salary: null,
+      cPos: null,
+      cNeededSkill: [],
+      pos: [
+        {
+          value: "前端工程師",
+          label: "前端工程師"
+        },
+        {
+          value: "後端工程師",
+          label: "後端工程師"
+        },
+        {
+          value: "資訊安全",
+          label: "資訊安全"
+        },
+        {
+          value: "資料庫管理",
+          label: "資料庫管理"
+        }
+      ],
+      Skill: [
+        {
+          value: "C++",
+          label: "C++"
+        },
+        {
+          value: "Javascript",
+          label: "Javascript"
+        },
+        {
+          value: "Html",
+          label: "Html"
+        },
+        {
+          value: "Python",
+          label: "Python"
+        },
+        {
+          value:"Nodejs",
+          label:"Nodejs"
+        },
+        {
+          value:"C#",
+          label:"C#"
+        }
+      ],
+      cPos: ""
+    };
+  },
+  methods: {
+    putwork() {
+      const axios = require('axios');
+      axios.post(
+        "http://163.13.226.86:23760/api/Job",
+        {
+          "Salary" : this.Salary,
+          "cPos" : this.cPos,
+          "cNeededSkill" : this.cNeededSkill
+        },
+        {
+          headers: {
+            "Content-Type": "application/json"
+            
+
+          }
+        }
+      )
+      .then(function(response) {
+        console.log(success);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+    },
+    addbox(){
+            this.cNeededSkill.push(null)
+            
+    },
+    delbox(index){
+           this.cNeededSkill.splice(index, index + 1) 
+        }
+    },
+
+  mounted () {
+      this.addbox()
+  }
+};
 </script>
--- src/components/Worker.vue
+++ src/components/Worker.vue
@@ -1,6 +1,7 @@
 <template>
      <div class="layout">
         <Layout :style="{minHeight: '100vh'}">
+        <div class="layout-header">
             <Header :style="{position: 'fixed', width: '100%'}">
                 <Menu mode="horizontal" theme="dark" active-name="1">
                     <div class="layout-logo">
@@ -12,33 +13,50 @@
                     </div>
                 </Menu>
             </Header>
+            </div>
             <Content :style="{margin: '88px 20px 0', background: '#fff', minHeight: '500px'}">
                 <div id="exp">
                     <font face="DFKai-sb" size="5">基本資料</font><br><br>
-                    <a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a><font face="DFKai-sb" size="3"><b>姓名:</b></font> <input type="text" placeholder="請輸入姓名..."><br><br>
-                    <a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a><font face="DFKai-sb" size="3"><b>E-mail:</b></font> <input type="text" placeholder="請輸入E-mail..."><br><br>
-                    <font face="DFKai-sb" size="3"><b>身分證ID:</b></font> <input type="text" placeholder="請輸入身分證ID..."><br><br>
-                    <font face="DFKai-sb" size="3"><b>期望薪水:</b></font> <input type="text" placeholder="請輸入期望薪水..."><br><br>
-                    <font face="DFKai-sb" size="3"><b>專長技能:</b></font> <input type="text" placeholder="請輸入專長技能..."><br><br>
+                    <a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+                    <font face="DFKai-sb" size="3"><b>姓名:</b></font> <input v-model="sName" type="text" placeholder="請輸入姓名..."><br><br>
+                    <a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+                    <font face="DFKai-sb" size="3"><b>E-mail:</b></font> <input type="text" v-model="sEmail" placeholder="請輸入E-mail..."><br><br>
+                    <font face="DFKai-sb" size="3"><b>身分證ID:</b></font> <input v-model="sID" type="text" placeholder="請輸入身分證ID..."><br><br>
+                    <font face="DFKai-sb" size="3"><b>期望薪水:</b></font> <input v-model="SalaryExpection" type="text" placeholder="請輸入期望薪水..."><br><br>
+                    <template>
+                    <div v-for="(sk, index) in Skill" v-bind:key="index">
+                        <font v-if="index==0" face="DFKai-sb" size="3">
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>技能:</b>
+                        </font>
+                        <font v-else>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</font>
+                        <Select v-model="Skill[index]" style="width:173px">
+                            <Option v-for="item in ability" :value="item.value" :key="item.value">{{ item.label }}</Option>
+                        </Select>
+                        <input type="button" value="刪除技能" v-on:click="delbox(index)" id="del"><br><br>
+                        <br>
+                    </div>
+                    <br><br>
+                    <input type="button" value="新增技能" v-on:click="addboxs" id="add"><br><br>
+                    </template>
                     <hr>
                     <font face="DFKai-sb" size="5">學歷</font><br><br>
-                    <font face="DFKai-sb" size="3"><b>最高學位:</b></font> <input type="text" placeholder="請輸入最高學位..."><br><br>
-                    <font face="DFKai-sb" size="3"><b>畢業學校:</b></font> <input type="text" placeholder="請輸入其學校..."><br><br>
-                    <font face="DFKai-sb" size="3"><b>畢業系所:</b></font> <input type="text" placeholder="請輸入其系所..."><br><br>
+                    <font face="DFKai-sb" size="3"><b>學位:</b></font> <input type="text" v-model="Degree" placeholder="請輸入最高學位..."><br><br>
+                    <font face="DFKai-sb" size="3"><b>學校:</b></font> <input type="text" v-model="School" placeholder="請輸入其學校..."><br><br>
+                    <font face="DFKai-sb" size="3"><b>系所:</b></font> <input type="text" v-model="Department" placeholder="請輸入其系所..."><br><br>
                     <hr>
                     <font face="DFKai-sb" size="5">工作經驗</font><br><br>
                     <Icon type="md-add" />
                     <input type="button" value="新增" v-on:click="addbox" id="add"><br><br>
                     <template id="expinput">
-                        <div v-for="(line, index) in lines" v-bind:key="index">
-                            <font face="DFKai-sb" size="3"><b>公司位址:</b></font> <input v-model="line.a" type="text" placeholder="請輸入你公司位址..."><br><br>
-                            <font face="DFKai-sb" size="3"><b>公司職位:</b></font> <input v-model="line.b" type="text" placeholder="請輸入職位..."><br><br>
-                            <font face="DFKai-sb" size="3"><b>在職時間:</b></font> <input v-model="line.c" type="text" placeholder="請輸入在職時間..."><br><br>
+                        <div v-for="(line, index) in WExp" v-bind:key="index">
+                            <font face="DFKai-sb" size="3"><b>公司名稱:</b></font> <input v-model="line.wPlace" type="text" placeholder="請輸入你公司名稱..."><br><br>
+                            <font face="DFKai-sb" size="3"><b>公司職位:</b></font> <input v-model="line.wPosition" type="text" placeholder="請輸入職位..."><br><br>
+                            <font face="DFKai-sb" size="3"><b>工作年資:</b></font> <input v-model="line.Duration" type="number" placeholder="請輸入工作年資..."><br><br>
                         </div>
                     </template>
                     <div id="button_check">
                         <router-link to="/">
-                            <Button type="primary" class="button button1">完成</Button>
+                            <Button type="primary" class="button button1" @click="putinfo()">完成</Button>
                         </router-link>
                     </div>
                     </br>
@@ -54,19 +72,109 @@
 export default{
     data () {
       return {
-        lines: []
+        WExp: [],
+        sID:null,
+        sName:null,
+        sEmail:null,
+        JobID:this.$route.params.id,
+        SalaryExpection:null,
+        Department:null,
+        School:null,
+        Degree:null,
+        Skill:[],
+        ability: [
+        {
+          value: "C++",
+          label: "C++"
+        },
+        {
+          value: "Javascript",
+          label: "Javascript"
+        },
+        {
+          value: "Html",
+          label: "Html"
+        },
+        {
+          value: "Python",
+          label: "Python"
+        },
+        {
+          value:"Nodejs",
+          label:"Nodejs"
+        },
+        {
+          value:"C#",
+          label:"C#"
+        }
+      ]
+    
       }
     },
     methods: {
+        addboxs(){
+            this.Skill.push(null)
+        },
+        delbox(index){
+           this.Skill.splice(index, index + 1) 
+        },
         addbox(){
-            this.lines.push({
-                a: null,
-                b: null,
-                c: null
+            this.WExp.push({
+                wPlace: null,
+                wPosition: null,
+                Duration: null
             })
+        },
+        putinfo() {
+      const axios = require('axios');
+      axios.post(
+        "http://163.13.226.86:23760/api/Jobseeker",
+        {
+          "sID" : this.sID,
+          "sName" : this.sName,
+          "sEmail" : this.sEmail,
+          "JobID": this.JobID
+        },
+        {
+          headers: {
+            "Content-Type": "application/json"
+          }
         }
+      )
+      .then(function(response) {
+        console.log(success);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+           axios.post(
+        "http://163.13.226.86:23760/api/CV",
+        {
+          "SalaryExpection" : parseInt(this.SalaryExpection),
+          "Department" : this.Department,
+          "School" : this.School,
+          "Degree" : this.Degree,
+          "Skill" : this.Skill,
+          "WExp": this.WExp
+        },
+        {
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }
+      )
+           .then(function(response) {
+        console.log(success);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
     },
-    mounted () {
+    
+    },
+     mounted () {
+    //  alert(this.$route.params.id)
+      this.addboxs()
       this.addbox()
     }
 }
@@ -125,6 +233,12 @@ border-radius: 5px; }
     position: relative;
     top: 5px;
     left: 20px;
+}
+.layout-header{
+  top: 0px;
+  width: 100%;
+  position: fixed;
+  z-index:10;
 }
 #exp{
     text-align: center;
